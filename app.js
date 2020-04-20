39e8e190984d0cd8c9d56ca814709b50a73b1a5c
ramon96 <rfm1@live.nl>
2020-04-20 14:15:28 +0200

    movement was added

--- app.js
+++ app.js
@@ -20,15 +20,23 @@ var socialRouter = require('./routes/social');
 const port = process.env.PORT || 3000;
 
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts'}));
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layouts'
+}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'src')));
 
@@ -37,41 +45,50 @@ app.use(indexRouter);
 app.use(socialRouter);
 
 let users = [];
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   // console.log('a user connected ' + socket.id);
-  
-  
+
+
   socket.emit('greet', users, socket.id)
-  console.log(users)
+  // console.log(users)
 
 
-  socket.on('adduser' , (data) => {
+  socket.on('adduser', (data) => {
     socket.broadcast.emit('adduser', data, socket.id)
     let userData = {
-      id: socket.id,
       data: data
     }
     users.push(userData)
+    console.log(users)
+    
   })
 
-    socket.on('disconnect', function(){
-      console.log(socket.id)
-      users = users.filter( user => user.id != socket.id)
-      console.log('usersdfasdf')
-      console.log(users)
-      socket.broadcast.emit('removeuser', users)
-    });
+  socket.on('updateuser', function(user){
+    let target = users.find(userList => {
+      return userList.data.id == user.id
+    })
+    target.data.x = user.x;
+    target.data.y = user.y;
+    
+    io.emit('updatelocations', users)
+  })
+
+  socket.on('disconnect', function () {
+    socket.id;
+    users = users.filter(user => user.data.id != socket.id)
+    socket.broadcast.emit('removeuser', users)
+  });
 
 });
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -87,4 +104,3 @@ server.listen(port, function () {
 });
 
 module.exports = app;
-
--- src/javascripts/rtw.js
+++ src/javascripts/rtw.js
@@ -1,85 +1,117 @@
 'use strict';
 
-(function(){
+(function () {
     let socket = io();
     let canvas = document.getElementsByClassName("area")[0];
     let ctx = canvas.getContext('2d');
-    let userId;
+    //let userId;
+
+    //client dot
+    let client = {
+        id: null,
+        x: 0,
+        y: 0
+    }
 
     // handling canvas size
     window.addEventListener('resize', onResize, false);
+    //set the canvas size on initialise
     onResize();
 
+    //Move the user on keypresses
+    window.onkeydown = function(e){
+        if(e.key == "a"){
+            client.x -= 10;
+        }
+        if(e.key == "s"){
+            client.y += 10;
+        }
+        if(e.key == "d"){
+            client.x += 10;
+        }
+        if(e.key == "w"){
+            client.y -= 10;
+        }
+
+
+        socket.emit('updateuser', client)
+
+    }
+
     //User needs to get other user data on connection so that we an place all the users
-    socket.on('greet', function(data, id){
-        // console.log(data)
-        userId = id;
+    socket.on('greet', function (data, id) {
+        //userId = id;
 
         let clientPosition = {
-        x : Math.floor(Math.random() * Math.floor(canvas.width)),
-        y : Math.floor(Math.random() * Math.floor(canvas.height))
+            x: Math.floor(Math.random() * Math.floor(canvas.width)),
+            y: Math.floor(Math.random() * Math.floor(canvas.height)),
+            id: id
         }
-
-
+        client.id = clientPosition.id;
+        client.x = clientPosition.x;
+        client.y = clientPosition.y;
+        
         // add the client user
         addUser(clientPosition, true)
-        console.log(data)
 
-
-        //there are users in on the server show them
+        //if there are users in on the server show them
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i].data)
             addUser(data[i].data, false)
-            
         }
-    }); 
+    });
 
 
-
-
-
-    // notices that there is another user, data contains user location
-    socket.on('adduser', function(data){
+    //the server tells the  client that there is another user, data contains user location 
+    socket.on('adduser', function (data) {
         addUser(data, false);
-        
+        console.log(data)
     })
 
-    socket.on('removeuser', function( data){
-        console.log('user gonee net als mijn geluk')
+    //the server tells the client someone disconnected, redraw the canvas without that user
+    socket.on('removeuser', function (data) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (let i = 0; i < data.length; i++) {
             addUser(data[i].data, false)
-            
+
         }
     })
-    
-    // makes user on the client
-    
-    function addUser(data, emit){
+
+    // when a user is moving
+    socket.on('updatelocations', function(users){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < users.length; i++) {
+             addUser(users[i].data, false)
+        }
+    })
+
+    // draw the user
+    function addUser(data, emit) {
         let radius = 25;
         let x = data.x;
         let y = data.y;
 
-        console.log(data)
-    
 
-    //     console.log('ik draw een user op ' +  x + " " + y)
-         ctx.beginPath();
-         ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-         ctx.fillStyle = '#F03C69';
-         ctx.fill();
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = '#F03C69';
+        ctx.fill();
 
-    // sends location to the server
-        if (!emit) { return; }
+        // sends location to the server
+        if (!emit) {
+            return;
+        }
         socket.emit('adduser', {
             x: x,
-            y: y
+            y: y,
+            id: data.id
         })
     }
 
 
 
-    function onResize(){
+    //window has been resized make an new canvas, user can ben outside of the view :P
+    function onResize() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     }
