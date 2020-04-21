c54e112f03f161fb638907e2cc8007123d921439
ramon96 <rfm1@live.nl>
2020-04-21 13:54:30 +0200

    image

--- routes/social.js
+++ routes/social.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 let coronaApi = require('../helpers/corona-api');
 
-coronaApi('summary')
-    .then(res => console.log(res))
+coronaApi('live/country/netherlands/status/confirmed')
+    .then(res => {
+        console.log(res)
+        // const netherlands = res.find(entry => {
+        //     console.log(entry)
+        //     // if(entry == "NL"){
+        //     //     return entry;
+        //     // }
+        // })
+        // console.log(netherlands)
+
+    })
 
 router.get('/social', function(req, res, next) {
     res.render('social', { title: 'social'});
