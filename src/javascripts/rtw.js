84b86873cf143cf37d527e9eea1b312c06316d20
ramon96 <rfm1@live.nl>
2020-05-01 15:57:08 +0200

    cant spawn on each other

--- src/javascripts/rtw.js
+++ src/javascripts/rtw.js
@@ -4,7 +4,7 @@
     let socket = io();
     let canvas = document.getElementsByClassName("area")[0];
     let ctx = canvas.getContext('2d');
-    //let userId;
+    let radius = 50;
 
     //client dot
     let client = {
@@ -41,10 +41,27 @@
     //User needs to get other user data on connection so that we an place all the users
     socket.on('greet', function (data, id) {
         //userId = id;
+   
+        // prevent overlap
+        // let clientX = Math.floor(Math.random() * Math.floor(canvas.width));
+        // let clientY =  Math.floor(Math.random() * Math.floor(canvas.height));
+        let clientX = randomIntFromRange(radius, canvas.width - radius);
+        let clientY = randomIntFromRange(radius, canvas.height - radius);
 
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].data)
+            if(distance(clientX, clientY, data[i].data.x, data[i].data.y) - radius * 2 < 0){
+                 clientX = randomIntFromRange(radius, canvas.width - radius);
+                 clientY = randomIntFromRange(radius, canvas.height - radius);
+
+                 i = -1;
+            }
+        }
+
+        
         let clientPosition = {
-            x: Math.floor(Math.random() * Math.floor(canvas.width)),
-            y: Math.floor(Math.random() * Math.floor(canvas.height)),
+            x: clientX,
+            y: clientY,
             id: id
         }
         client.id = clientPosition.id;
@@ -82,11 +99,25 @@
         for (let i = 0; i < users.length; i++) {
              addUser(users[i].data, false)
         }
+
+        // voor de collision detection kan ik me user ide gebruiken om te achterhalen welk stipje de client is
+
+        for (let i = 0; i < users.length; i++) {
+            // voorkomt collision check met zichzelf
+            if(client.id === users[i].data.id){
+                continue;
+            }
+            // collision check
+            if(distance(client.x, client.y, users[i].data.x, users[i].data.y) - radius * 2 < 0){
+                console.log('collision detected')
+           }
+
+        }
     })
 
     // draw the user
     function addUser(data, emit) {
-        let radius = 25;
+
         let x = data.x;
         let y = data.y;
 
@@ -107,7 +138,20 @@
         })
     }
 
+    // utility function from https://github.com/christopher4lis/canvas-boilerplate/blob/master/src/js/utils.js
+    function randomIntFromRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
 
+    // utility function from https://github.com/christopher4lis/canvas-boilerplate/blob/master/src/js/utils.js
+    function distance(x1, y1, x2, y2) {
+        const xDist = x2 - x1
+        const yDist = y2 - y1
+        
+      //https://nl.wikipedia.org/wiki/Stelling_van_Pythagoras
+        return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+      }
+      
 
     //window has been resized make an new canvas, user can ben outside of the view :P
     function onResize() {
@@ -117,3 +161,9 @@
     }
 
 })();
+
+
+// Todo
+// voeg bots toe die een random velocity krijgen
+// laat de bots bouncen als ze uit het de canvas width gaan
+// hoeveelheid bots laten bepalen op basis van de coronavirus data
